cc8718f2ee9442908c3b67058d5d6035f5bce7e4
Siravitt <siravitt.tan@gmail.com>
2024-07-24 15:28:11 +0700

    :sparkles: azure-login

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+export const authOptions = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // IMPORTANT: Persist the access_token to the token right after sign in
+      if (account) {
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+  },
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
